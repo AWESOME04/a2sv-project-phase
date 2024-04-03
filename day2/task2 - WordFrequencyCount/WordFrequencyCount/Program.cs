a1c379b265c7576f3e3682352a32bd8bc2040420
Evans Acheampong <evansachie01@gmail.com>
2024-04-03 22:43:47 +0000

    day2 - task2

---
+++ day2/task2 - WordFrequencyCount/WordFrequencyCount/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class WordFrequencyCount
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a word: ");
+        string word = Console.ReadLine();
+
+        Dictionary<char, int> counter = new Dictionary<char, int>();
+
+        foreach (char c in word.ToLower())
+        {
+            if (char.IsLetter(c))
+            {
+                if (counter.ContainsKey(c))
+                {
+                    counter[c]++;
+                }
+                else
+                {
+                    counter[c] = 1;
+                }
+            }
+        }
+
+        Console.WriteLine("{");
+        foreach (var kvp in counter)
+        {
+            Console.WriteLine($" '{kvp.Key}': {kvp.Value},");
+        }
+        Console.WriteLine("}");
+    }
+}
