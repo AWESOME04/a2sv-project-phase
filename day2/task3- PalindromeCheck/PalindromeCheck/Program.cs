135ffe3a9a12d0fc548622575013b018d9f0916e
Evans Acheampong <evansachie01@gmail.com>
2024-04-04 00:06:53 +0000

    day2 task3

---
+++ day2/task3- PalindromeCheck/PalindromeCheck/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+class PalindromeCheck
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a word: ");
+        string palindrome = Console.ReadLine();
+
+        if (isPalindrome(palindrome))
+        {
+            Console.WriteLine("It is a palindrome.");
+        }
+        else
+        {
+            Console.WriteLine("It is not a palindrome.");
+        }
+
+    }
+
+    static bool isPalindrome(string input)
+    {
+        string lowerInput = input.ToLower();
+
+        char[] charArray = lowerInput.ToCharArray();
+        Array.Reverse(charArray);
+        string reversed = new string(charArray);
+
+        return lowerInput == reversed;
+    }
+}
